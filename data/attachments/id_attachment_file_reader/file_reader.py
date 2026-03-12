70f31629d387dbe56df5e47b1a48a642665c792e
uuulf91 <uuulf91@users.noreply.github.com>
2026-03-12 17:17:08 +0100

    V1.1.0 update
    - changed folder structure of all proof-environment data
    - updated general architecture of proof
    - updated sync strategies
    - fix several bugs

--- data/attachments/id_attachment_file_reader/file_reader.py
+++ data/attachments/id_attachment_file_reader/file_reader.py
@@ -13,12 +13,12 @@ import os
 options, arguments = cliargparser.parse_known_args()
 
 # Use of the custom PROOF logger. Each file has its own logger.
-log_file_name = "proof_FileLineProviderLogger_" + str(options.local_block_id) + ".log"
-logger = Logger('FileLineProviderLogger', handlers = [HandlerType.FILE], logging_dir=options.loggingDir, log_file_name = log_file_name, log_level=options.logLevel).get_logger()
+log_file_name = "proof_FileReaderLogger_" + str(options.local_block_id) + ".log"
+logger = Logger('FileReaderLogger', handlers = [HandlerType.FILE], logging_dir=options.loggingDir, log_file_name = log_file_name, log_level=options.logLevel).get_logger()
 
-class FileLineProvider(BaseWrapper):
+class FileReader(BaseWrapper):
     """
-    The FileLineProvider module is a Python Wrapper for PROOF that allows users to read lines from a file and provide this line as output.
+    The FileReader module is a Python Wrapper for PROOF that allows users to read lines from a file and provide this line as output.
     The attribute `file_name` is set to the file path, and the file is opened in read mode.
     the attribute `ignore_first_line` can be set to True to skip the first line of the file. This is often useful for files with headers, e.g. CSV files.
     the attribute `num_steps` defines how many lines should be read from the file and provided. If the end of the file is reached before this number, the execution will finish.
@@ -29,7 +29,7 @@ class FileLineProvider(BaseWrapper):
         self.communication_point=-1     # local communication point for debugging, set by a value message before step() by the Worker
         self.currentStep = 0            # local current step number
         self.currentRow = 0             # local current row number
-        self.num_steps = 10             # number of lines to read from the file, can be set in the PROOF Block/Template
+        self.num_steps = -1             # max number of steps to perform reading one line from the file, can be set in the PROOF Block/Template
         self.line = None                # local variable to store the current line read from the file, set in step()
         self.file_name = None           # STEPBASED_STATIC, required input, set in init()
         self.file_path = None           # local path to the file to be read, set in init()
@@ -38,11 +38,11 @@ class FileLineProvider(BaseWrapper):
         self.ignore_first_line = "false"# can be set to True to ignore the first line of the file, e.g. for CSV files with headers
         self._ignore_first_line = False # internal flag to ignore the first line, default is False
 
-        super(FileLineProvider, self).__init__(bwoptions=opt)
+        super(FileReader, self).__init__(bwoptions=opt)
 
     async def init(self) -> None:
-        logger.debug("initializing FileLineProvider, Values given:")
-        self.file_path = os.path.join(self.workspace_directory, self.file_name)
+        logger.debug("initializing FileReader, Values given:")
+        self.file_path = os.path.join(self.userdata_directory, self.file_name)
         logger.debug("file_name: " + str(self.file_path))
         logger.debug("inputs: " + str(self.inputs))
         logger.debug("outputs: " + str(self.outputs))
@@ -56,7 +56,7 @@ class FileLineProvider(BaseWrapper):
             if self._ignore_first_line:
                 self.file.readline()
 
-            await super(FileLineProvider, self).init()
+            await super(FileReader, self).init()
 
         except Exception as e:
             error_txt = "Error opening file '" + self.file_path + "' -> " + str(e) + "\n" + str(traceback.format_exc())
@@ -68,19 +68,25 @@ class FileLineProvider(BaseWrapper):
         self.currentStep += 1
         logger.debug(f"processing STEP(CS: {self.currentStep};  num_steps: {self.num_steps})")
         try:
-            if self.currentStep > self.num_steps:
-                logger.debug(f"processing STEP; given number of steps reached ({self.num_steps}) -> EXECUTION FINISHED ")
-                await super(FileLineProvider, self).step(BlockStatus.EXECUTION_FINISHED)
+            if self.num_steps >= 0 and self.currentStep > self.num_steps:
+                logger.info(f"processing STEP {self.currentStep}; given number of steps reached ({self.num_steps}) -> EXECUTION FINISHED ")
+                await super(FileReader, self).step(BlockStatus.EXECUTION_FINISHED)
                 return
 
             if self.file is not None:
                 self.line = self.file.readline()
                 self.currentRow += 1
+                # Check when EOF is reached (empty string returned, empty line would be '\n')
+                eofReached = self.line == ''
+                if eofReached:
+                    logger.info(f"processing STEP {self.currentStep}; end of file reached after {self.currentRow} rows -> EXECUTION FINISHED ")
+                    await super(FileReader, self).step(BlockStatus.EXECUTION_FINISHED)
+                    return
             else:
                 raise ValueError("Error reading file '" + self.file_path + "': file is not opened yet")
 
             logger.debug(f"LINE for CP {self.communication_point}: \n" + str(self.line))
-            await super(FileLineProvider, self).step(BlockStatus.EXECUTION_STEP_FINISHED)
+            await super(FileReader, self).step(BlockStatus.EXECUTION_STEP_FINISHED)
 
         except Exception as e:
             error_txt = "Error in step() reading file line of '" + self.file_path + "' -> " + str(e) + "\n" + str(traceback.format_exc())
@@ -88,19 +94,19 @@ class FileLineProvider(BaseWrapper):
             await self.send_notify(SimulationPhase.EXECUTE, BlockStatus.ERROR_STEP, error_txt)
 
     async def finalize(self) -> None:
-        logger.debug("finalize() -> FileLineProvider executed the finalize method")
-        if self.file is not None:
+        logger.debug("finalize() -> FileReader executed the finalize method")
+        if self.file is not None and not self.file.closed:
             self.file.close()
             self.file = None
         else:
             raise ValueError("ERROR closing file '" + self.file_path + "':  File is not opened yet")
-        await super(FileLineProvider, self).finalize()
+        await super(FileReader, self).finalize()
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main(wrapper=FileLineProvider()))
-        logger.debug("The main method of the file_line_provider.py gets executed!")
+        asyncio.run(main(wrapper=FileReader()))
+        logger.debug("The main method of the file_reader.py gets executed!")
     except KeyboardInterrupt:
         exit(0)
 
--- data/attachments/id_attachment_file_writer/file_writer.py
+++ data/attachments/id_attachment_file_writer/file_writer.py
@@ -19,7 +19,6 @@ import os
 
 options, arguments = cliargparser.parse_known_args()
 
-
 # Use of the custom PROOF logger. Each file has its own logger.
 log_file_name = "proof_FileWriterLogger_" + str(options.local_block_id) + ".log"
 logger = Logger('FileWriterLogger', handlers = [HandlerType.FILE], logging_dir=options.loggingDir, log_file_name = log_file_name, log_level=options.logLevel).get_logger()
@@ -34,10 +33,8 @@ class FileWriter(BaseWrapper):
         self.file_path = None      # local path to the file to be written to, set in init()
         self.file = None           # file object, opened in init()
         self.mode = 'w'            # write mode, can be 'w' for write or 'a' for append, set in init()
-        self.waitForSync = None    # wait for a SYNC before writing the data, can be set in the PROOF Block/Template
-        self._wait_for_sync = True # internal flag to wait for a SYNC, default is True
         self.additionalCRLF = None # if True, an additional Carrage Return and Line Feed will be printed. This can be important when values without CRLF should be written
-        self._additional_clrf = False # internal flag for given worker value
+        self._additional_crlf = False # internal flag for given worker value
         self.data = ""             # data to be written to the file, set in step()
         #logger.debug("__init__() -> FileWriter initialized \n")
 
@@ -45,16 +42,20 @@ class FileWriter(BaseWrapper):
 
     async def init(self) -> None:
         logger.debug("initializing FileWriter, Values given:")
-        self.file_path = os.path.join(self.workspace_directory, self.file_name)
+        self.file_path = os.path.join(self.userdata_directory, self.file_name)
+        parent_path = os.path.dirname(self.file_path)
+        if not os.path.exists(parent_path):
+            logger.info(f"Parent path '{parent_path}' does not exist, creating folders")
+            os.makedirs(name=parent_path, exist_ok=True)
         logger.debug("filename: " + str(self.file_path))
         logger.debug("inputs: " + str(self.inputs))
         logger.debug("outputs: " + str(self.outputs))
 
-        self._wait_for_sync = self.waitForSync.lower() == "true" if self.waitForSync is not None else False
-        self._additional_clrf = self.additionalCRLF.lower() == "true" if self.additionalCRLF is not None else False
-        logger.debug("waitForSync: " + str(self._wait_for_sync) + ",  additionalCRLF: " + str(self._additional_clrf) )
+        self._additional_crlf = self.additionalCRLF.lower() == "true" if self.additionalCRLF is not None else False
+        logger.debug("additionalCRLF: " + str(self._additional_crlf) )
 
         try:
+            logger.debug(f"Opening file {os.path.abspath(self.file_path)}")
             self.file = open(self.file_path, self.mode)
             await super(FileWriter, self).init()
         except Exception as e:
@@ -69,7 +70,7 @@ class FileWriter(BaseWrapper):
         try:
             if self.data is not None:
                 self.file.write(self.data)
-                if self._additional_clrf:
+                if self._additional_crlf:
                     self.file.write("\n")
                 if self.mode == 'a':
                     logger.debug(f"processing STEP; data appended to file '{self.data}'")
@@ -77,36 +78,32 @@ class FileWriter(BaseWrapper):
                     logger.debug(f"processing STEP; data written to file '{self.data}'")
                 await super(FileWriter, self).send_notify(SimulationPhase.EXECUTE, BlockStatus.EXECUTION_STEP_FINISHED, error_text="")
         except Exception as e:
-            error_txt = "Error writing to file '" + self.file_path + "' -> " + str(e) + "\n" + str(traceback.format_exc())
+            error_txt = "Error writing to file '" + str(self.file_path) + "' -> " + str(e) + "\n" + str(traceback.format_exc())
             logger.error(error_txt)
             await self.send_notify(SimulationPhase.EXECUTE, BlockStatus.ERROR_INIT, error_txt)
 
     async def finalize(self) -> None:
         logger.debug("executing the finalize method")
-        if self.file is not None:
+        if self.file is not None and not self.file.closed:
             self.file.close()
             self.file = None
         else:
             raise ValueError("ERROR closing file '" + self.file_path + "':  File is not opened yet")
         await super(FileWriter, self).finalize()
 
-    # overwriting the following BaseWrapper function to process wait_for_sync and
-    # send the data immediately if not waiting for a SYNC
-    async def set_variables(self, variables: Dict, phase: SimulationPhase) -> None:
-        for key, value in variables.items():
-            setattr(self, key, value)
-
-        if self._wait_for_sync:
-            logger.debug( "waiting for SYNC, then sending value, CP: "  + str(self.communication_point))
-        else:
-            logger.debug( "Do not wait for a SYNC, sending value and notify at once ..." )
-            await self.step()
+    async def shut_down(self, status=None, error_text="") -> None:
+        logger.debug("executing the shut_down method")
+        # Ensure that the file is being closed is if is still open
+        if self.file is not None and not self.file.closed:
+            self.file.close()
+            self.file = None
+        await super(FileWriter, self).shut_down()
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main(wrapper=FileWriter()))
         logger.debug("The main method of the file_line_provider.py gets executed!")
+        asyncio.run(main(wrapper=FileWriter()))
     except KeyboardInterrupt:
         exit(0)
 
